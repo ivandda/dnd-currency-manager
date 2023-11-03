609c9e306c330e3efc8adeccafbb358a5703b944
ivandda <ivandominguezdealzaga@gmail.com>
2023-11-03 14:29:55 -0300

    change db

--- app/database/database.py
+++ app/database/database.py
@@ -3,14 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # local DB
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/fastAPIwallet"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/fastAPIwallet"
 
 # docker db
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
 
 
 # supabase db
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qK6jusID5tarUzg9@db.apdtlbxlcjdkimvoqqse.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qK6jusID5tarUzg9@db.apdtlbxlcjdkimvoqqse.supabase.co:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
