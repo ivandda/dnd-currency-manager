bcad4432293f55324353eeef410bc5151c6d7cc2
ivandda <ivandominguezdealzaga@gmail.com>
2023-12-10 01:53:47 -0300

    delete old router

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from starlette.responses import RedirectResponse
 from app.database.database import engine
 from app.dependencies import tags_metadata, swagger_ui_parameters
 from app.models import models
-from app.routers import home, characters, parties, character_transaction, money_parties, money_dm, money_character
+from app.routers import home, characters, parties, money_parties, money_dm, money_character
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -25,7 +25,6 @@ async def redirect_to_home_page():
 app.include_router(home.router)
 app.include_router(characters.router)
 app.include_router(parties.router)
-app.include_router(character_transaction.router)
 app.include_router(money_parties.router)
 app.include_router(money_dm.router)
 app.include_router(money_character.router)
